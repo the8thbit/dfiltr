f965218fdf3f6690c5cffb56fa8f8b3b4b27afdf
Peter Sussman <pj.sussman@gmail.com>
2014-02-04 02:30:17 -0600

    sigh

--- client/chat.js
+++ client/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://localhost:8080') || io.connect(process.env.OPENSHIFT_NODEJS_IP + ":" + process.env.OPENSHIFT_NODEJS_PORT);
+    var socket = io.connect(process.env.OPENSHIFT_NODEJS_IP + ":" + process.env.OPENSHIFT_NODEJS_PORT);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
