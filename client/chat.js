1d2769a257ece75af233d234e28e1a3df694d75a
Peter Sussman <pj.sussman@gmail.com>
2014-02-04 02:35:09 -0600

    Take four...

--- client/chat.js
+++ client/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect(process.env.OPENSHIFT_NODEJS_IP + ":" + process.env.OPENSHIFT_NODEJS_PORT);
+    var socket = io.connect(process.env.OPENSHIFT_NODEJS_IP);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
