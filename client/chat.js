297fa62ea1424176d6060a066d472ca37be8b032
Peter Sussman <pj.sussman@gmail.com>
2014-02-04 23:55:41 -0600

    changed console output

--- client/chat.js
+++ client/chat.js
@@ -8,21 +8,21 @@ window.onload = function() {
 	var content = document.getElementById("content");
  
 	socket.on('message', function (data) {
-		if(data) {
-			messages.push(data);
+		if(data.message) {
+			messages.push(data.message);
 			var html = '';
 			for(var i=0; i<messages.length; i++) {
 				html += messages[i] + '<br />';
 			}
 			content.innerHTML = html;
 		} else {
-			console.log("There is a problem:", data);
+			console.log("error sending message:", data);
 		}
 	});
  
 	sendButton.onclick = function() {
 		var text = field.value;
-		console.log("There is a problem:", text);
+		console.log("you: ", text);
 		socket.emit('send', { message: text });
 	};
 }
