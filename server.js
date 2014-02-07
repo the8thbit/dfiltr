ff61a6934579c0b9396e2837c094b0da6e85d165
Peter Sussman <pj.sussman@gmail.com>
2014-02-07 17:17:06 -0600

    switched to using sockets for identifying users

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 //====================================
 //IP ADDRESS 
   var IP_ADD =
-//'localhost'; //toggle on for local testing
-  process.env.OPENSHIFT_NODEJS_IP; //toggle on for openshift deploy
+  'localhost'; //toggle on for local testing
+//process.env.OPENSHIFT_NODEJS_IP; //toggle on for openshift deploy
 //======================================
 //PORT NUMBER
   var PORT_NUM = 
-//8080; //toggle on for local testing
+  8080; //toggle on for local testing
 //80;
-  process.env.OPENSHIFT_NODEJS_PORT; //toggle on for openshift deploy
+//process.env.OPENSHIFT_NODEJS_PORT; //toggle on for openshift deploy
 //====================================
 
 //use express
@@ -39,16 +39,19 @@ io.configure(function() {
 console.log('listening at ' + IP_ADD + ' on port ' + PORT_NUM); //listening at IP on port PORT
 
 //chat protocol code
-var numUsers = 0;
+var connections = [];
 
 io.sockets.on('connection', function (socket) {
-	numUsers++;
+	connections.push(socket);
 	console.log('user has connected');
-	socket.emit('setid', { id: numUsers });
-	socket.emit('message', { message: 'welcome to the chat, user number ' + numUsers });
+	socket.emit('message', { message: 'welcome to the chat.' });
 
 	//when server recieves 'send' send 'message' to clients 
 	socket.on('send', function (data) {
-		io.sockets.emit('message', data);
+		for(var i = 0; i < connections.length; i++) {
+			if(socket != connections[i] ) { 
+				connections[i].emit('message', data);
+			}
+		}
 	});
 });
