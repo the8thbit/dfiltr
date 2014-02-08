a27e3055942338da65cad4a169c72df30bb523b4
Peter Sussman <pj.sussman@gmail.com>
2014-02-08 17:17:36 -0600

    fixed some style things

--- client/chat.js
+++ client/chat.js
@@ -1,5 +1,3 @@
-window.onload = function() {
-
 //============================
 //CLIENT CONFIGURATION
 //============================
@@ -13,57 +11,50 @@ window.onload = function() {
   8080; //use with localhost
 //80;
 //8000; //neccessary for openshift, as websockets is restricted to this port
-//---------------------------- 
+//----------------------------
 
+window.onload = function() {
 	var messages = [];
-	var socket = io.connect("http://" + IP_ADD + ":" + PORT_NUM + "/" );
-	var field = document.getElementById("chat-input-field");
-	var sendButton = document.getElementById("chat-input-send");
-	var content = document.getElementById("chat-output");
+	var socket = io.connect( 'http://' + IP_ADD + ':' + PORT_NUM + '/' );
+	var field = document.getElementById( 'chat-input-field' );
+	var sendButton = document.getElementById( 'chat-input-send' );
+	var content = document.getElementById( 'chat-output' );
  
-	$("textarea").keydown(function(e){
-		if (e.keyCode == 13 && !e.shiftKey) {
+	$( 'textarea' ).keydown( function( e ){
+		if( e.keyCode == 13 && !e.shiftKey ) {
 			e.preventDefault();
 			addMessage( field.value );
 			sendMessage( field.value );
 		}
-	}); 
+	} ); 
 
 	sendButton.onclick = function() {
 		addMessage( field.value );
 		sendMessage( field.value );
 	};
 
-	socket.on('message', function (data) {
-		if(data.message && data.message != '') {
-			messages.push(data.message);
-			var html = '';
-			for(var i=0; i<messages.length; i++) {
-				html += messages[i] + '<br />';
-			}
-			content.innerHTML = html;
-		} else {
-			console.log("error transporting message:", data);
-		}
+	//what to do when the user recieves a message
+	socket.on( 'message', function( data ) {
+		addMessage( data.message );
 	});
  
 	addMessage = function( text ) {
 		if( text && text != '' ) {
 			messages.push(text);
-				var html = '';
-				for(var i=0; i<messages.length; i++) {
-					html += messages[i] + '<br />';
-				}
+			var html = '';
+			for( var i=0; i < messages.length; i++ ) {
+				html += messages[i] + '<br />';
+			}
 			content.innerHTML = html;
 		} else {
-			console.log("error transporting message:", data);
+			console.log( 'error transporting message:', data );
 		}
 	};
 
 	sendMessage = function( text ) {
 		field.value = '';
 		content.scrollTop = content.scrollHeight * 10;
-		socket.emit('send', { message: text });
-		console.log("you: ", text);
+		socket.emit( 'send', { message: text } );
+		console.log( 'you: ', text );
 	}
 }
--- server.js
+++ server.js
@@ -13,85 +13,106 @@
 //process.env.OPENSHIFT_NODEJS_PORT; //toggle on for openshift deploy
 //====================================
 
+//====================================
+//SERVER START UP
+//====================================
 //use express
-var express = require('express');
+var express = require( 'express' );
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use( express.static( __dirname + '/' ) );
 
 //use jade templates for HTML and CSS
-app.set('views', __dirname + '/tpl');
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express);
-app.get('/', function(req, res){
-	res.render('page');
-});
+app.set( 'views', __dirname + '/tpl' );
+app.set( 'view engine', 'jade' );
+app.engine( 'jade', require( 'jade' ).__express );
+app.get( '/', function( req, res ){ res.render( 'page' ); } );
 
-//give node a location to listen on 
-var io = require('socket.io').listen(
-	app.listen(PORT_NUM, IP_ADD)
-);
+//use socket.io and give it a location to listen on 
+var io = require( 'socket.io' ).listen( app.listen(PORT_NUM, IP_ADD) );
 
 //turn websockets on
-io.configure(function() {
-	io.set('transports', ['websocket']);
-});
+io.configure( function() {
+	io.set( 'transports', [ 'websocket' ] );
+} );
 
-console.log('listening at ' + IP_ADD + ' on port ' + PORT_NUM); //listening at IP on port PORT
+console.log( 'listening at ' + IP_ADD + ' on port ' + PORT_NUM ); 
 
-//chat protocol code
+//====================================
+//CHAT PROTOCOL
+//====================================
 var sockets = []; //list of all users
-var pool = [];    //pool of unconnected users
+var pool = [];    //pool of unpaired users
 
-io.sockets.on('connection', function (socket) {
-	console.log('user connecting...');
-	console.log('| user added to list of users at position ' + sockets.push(socket));
-	socket.emit('message', { message: 'welcome to the chat.' });
+//what to do when the socket connects (this also bootstraps the rest of the protocol)
+io.sockets.on( 'connection', function( socket ) {
+	console.log( 'user connecting...' );
+	socket.pos = sockets.push( socket ); //add socket to the server's list of sockets
+	console.log( '| user added to list of users at position ' + sockets.length );
+	socket.emit( 'message', { message: 'welcome to the chat.' } );
 
+	//if there are users in the pool, take one of them and make them your
+	//partner. If not, jump in the pool.
 	if( pool.length ) {
+		console.log( '| partner found in pool' );
 		var partner = pool.shift();
 		partner.pool = null;
-		socket.partner = partner;
-		partner.partner = socket;
-		socket.partner.emit('message', { message: 'You\'ve been paired with a user.' });
-		partner.partner.emit('message', { message: 'You\'ve been paired with a user.' });
+		console.log( '| partner removed from pool. Current pool size is: ' + pool.length );
+		socket.partner = partner; //adds a '.partner' var to the socket which can be used to
+		partner.partner = socket; //access the user's conversational partner
+		socket.partner.emit( 'message', { message: 'You\'ve been paired with a user.' } );
+		partner.partner.emit( 'message', { message: 'You\'ve been paired with a user.' } );
 	} else {
-		socket.pool = pool.push(socket);
-		socket.emit('message', { message: 'Looking for a partner...' });
+		console.log( '| partner could not be found in pool' );
+		socket.pool = pool.push( socket );
+		console.log( '| added user to pool at position ' + socket.pool );
+		socket.emit( 'message', { message: 'Looking for a partner...' } );
 	}
 
-	//when server recieves 'send' relay 'message' to client 
-	socket.on('send', function (data) {
-		if( socket.partner ) {
-			socket.partner.emit('message', data);
-		} else {
-			socket.emit('message', { message: 'We are having technical difficulties. You don\'t have a partner, but you think you do. Basically, you\'re far more alone than you believe. Let\'s get you someone new to talk to.' });
-			if( !socket.pool ) {
-				socket.pool = pool.push(socket); 
-			}  
-			socket.emit('message', { message: 'Looking for a partner...' });
-		}
-	});
+	console.log( 'user connected' );
 
-	socket.on('disconnect', function () {		
-		console.log('user disconnecting...');
+	//what to do when the user disconnects
+	socket.on( 'disconnect', function() {		
+		console.log( 'user disconnecting...' );
 
 		if( socket.pool ) {
-			console.log("| user is in pool at position " + socket.pool );
-			console.log("| current size of pool is " + pool.length);
-			console.log("| removing user from pool...");	
-			pool.splice( socket.pool - 1, 1);
+			console.log( '| user is in pool at position ' + socket.pool );
+			console.log( '| current size of pool is ' + pool.length );
+			console.log( '| removing user from pool...' );	
+			pool.splice( socket.pool - 1, 1 );
 			socket.pool = null;
-			console.log("| current size of pool is " + pool.length);
+			console.log( '| current size of pool is ' + pool.length );
 		}
 
 		if( socket.partner ) {
-			console.log("| user has a conversational partner");
-			console.log("| disconnecting user from partner...");
-			socket.partner.emit('message', { message: 'Your conversational partner has disconnected.' });
+			console.log( '| user has a conversational partner' );
+			console.log( '| disconnecting user from partner...' );
+			socket.partner.emit( 'message', { message: 'Your conversational partner has disconnected.' } );
 			socket.partner.partner = null;
 			socket.partner = null;
 		}
 
-		console.log('user disconnected');
-	});
-});
+		if( socket.pos ) {
+			console.log( '| user is socket number ' + socket.pos );
+			console.log( '| current number of sockets is ' + sockets.length );
+			console.log( '| removing user from list of sockets...' );	
+			sockets.splice( socket.pos - 1, 1);
+			socket.pos = null;
+			console.log( '| current number of sockets is ' + sockets.length );
+		}
+
+		console.log( 'user disconnected' );
+	} );
+
+	//when server recieves 'send' relay 'message' to client
+	//'send' is a chat message coming from a client, and
+	//'message' is a chat message being sent from the server to a client.
+	socket.on( 'send', function( data ) {
+		if( socket.partner ) {
+			socket.partner.emit( 'message', data );
+		} else {
+			socket.emit( 'message', { message: 'We are having technical difficulties. You don\'t have a partner, but you think you do. Basically, you\'re far more alone than you believe. Let\'s get you someone new to talk to.' } );
+			if( !socket.pool ) { socket.pool = pool.push( socket ); }  
+			socket.emit( 'message', { message: 'Looking for a partner...' } );
+		}
+	} );
+} );
