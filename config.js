9886c9c116d0b205cc432436ce7bcdc2d1ebf3b3
Peter Sussman <pj.sussman@gmail.com>
2014-02-12 10:13:42 -0600

    switched to deploy mode

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 //=====================================//
 //        SERVER CONFIGURATION         //
 //=====================================//
-//if( process.env ) {                  // TOGGLE ON FOR OPENSHIFT DEPLOY
+  if( process.env ) {                  // TOGGLE ON FOR OPENSHIFT DEPLOY
 //=====================================//
 //IP ADDRESS                           //
   var SERVER_IP =                      //
-  'localhost';                         // toggle on for local testing
-//process.env.OPENSHIFT_NODEJS_IP;     // toggle on for openshift deploy
+//'localhost';                         // toggle on for local testing
+  process.env.OPENSHIFT_NODEJS_IP;     // toggle on for openshift deploy
 //=====================================//
 //PORT NUMBER                          //
   var SERVER_PORT =                    //
-  8080;                                // toggle on for local testing
-//process.env.OPENSHIFT_NODEJS_PORT;   // toggle on for openshift deploy
+//8080;                                // toggle on for local testing
+  process.env.OPENSHIFT_NODEJS_PORT;   // toggle on for openshift deploy
 //=====================================//
-//}                                    // TOGGLE ON FOR OPENSHIFT DEPLOY
+  }                                    // TOGGLE ON FOR OPENSHIFT DEPLOY
 //=====================================//
 
 //=====================================//
@@ -21,14 +21,18 @@
 //=====================================//
 //IP ADDRESS                           //
   var CLIENT_IP =                      //
-  'localhost';                         // toggle on for local testing
-//'435-teamnoname.rhcloud.com';        // toggle on for openshift deploy
+//'localhost';                         // toggle on for local testing
+  '435-teamnoname.rhcloud.com';        // toggle on for openshift deploy
 //=====================================//
 //PORT NUMBER                          //
   var CLIENT_PORT =                    //
-  8080;                                // toggle on for local testing
-//8000;                                // neccessary for openshift, as websockets is restricted to this port
+//8080;                                // toggle on for local testing
+  8000;                                // neccessary for openshift, as websockets is restricted to this port
 //=====================================//
+
+
+
+
 
 if( process == 'SERVER' || process.env ) {
 	module.exports.CLIENT_IP   = CLIENT_IP;
