d49aa323a402129183cd27aec477df162b012d84
Peter Sussman <the8thbit@gmail.com>
2014-02-28 15:55:48 -0600

    switched to deploy mode

--- schemas/mainDB.js
+++ schemas/mainDB.js
@@ -2,10 +2,8 @@
 //CONNECTS TO MONGODB AND LOADS UP ALL SCHEMAS
 //NOTE: HERE IS WHERE WE CAN CREATE NEW USERS AND SAVE TO DATABASE
 //====================================
-var config  = require( './config.js' );
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://' + SERVER_IP + '/chatappdb');
+mongoose.connect('mongodb://' + config.SERVER_IP + ':' 27017 + '/chatappdb');
 User = require('./user-schema.js');
 
 var NewUser = new User({
