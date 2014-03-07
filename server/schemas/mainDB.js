22fc815b1f9bf01cad3adef982ad537227b32371
Ariel Godinez <ariel@ariel-Aspire-7741.(none)>
2014-03-07 13:34:55 -0600

    added password hashing and some test db functions

--- server/schemas/mainDB.js
+++ server/schemas/mainDB.js
@@ -14,9 +14,64 @@ mongo.on( 'error', console.error.bind( console, 'mongo error:' ) );
 //=============================================================================
 User = require( './user-schema.js' );
 
+//create a new user
 var newUser = new User( {
-	username: 'testUser1',
-	password: 'testPassword1'
+	username: 'testUser3',
+	password: 'testPassword3'
 });
 
-newUser.save();
+// save new user to mongodb
+newUser.save( function( err ) {
+	if ( err ) throw err;
+
+    	// fetch user and test password verification
+	User.findOne( { username: 'testUser3' }, function( err, user ) {
+ 		if ( err ) throw err;
+
+        	// test a matching password
+		user.comparePassword( 'testPassword3', function( err, isMatch ) {
+			if ( err ) throw err;
+			console.log( 'testPassword3', isMatch ); 
+		});
+
+		// test a failing password
+		user.comparePassword('123Password', function(err, isMatch) {
+			if (err) throw err;
+			console.log('123Password:', isMatch);
+        	});
+    	});
+});
+
+
+
+//=============================================================================
+//SOME QUERYING AND OTHER DB FUNCTIONS
+//=============================================================================
+
+/*
+
+
+//query document-- working
+User.find( {username: "testUser1" }, function( err, users ) {
+	if( err || !users ) console.log( "User not found!" );
+  		else users.forEach( function( foundUser ) {
+    		console.log( 'USERNAME: %s \nPASSWORD: %s', foundUser.username,foundUser.password );
+	});
+});
+
+
+//update a document-- working
+User.update( { username: "testUser1" }, { $set: { password: "changedpassword" } }, function( err, updated ) {
+  	if( err || !updated ) console.log( "User not updated!" );
+  		else console.log( "User updated!" );
+});
+
+
+
+*/
+
+
+
+
+
+
--- server/schemas/user-schema.js
+++ server/schemas/user-schema.js
@@ -1,12 +1,50 @@
 //=============================================================================
-//VERY SIMPLE USERNAME AND PASSWORD SCHEMA
+//VERY SIMPLE USERNAME AND PASSWORD SCHEMA WITH PASSWORD HASH
 //=============================================================================
 var mongoose = require( 'mongoose' );
-var schema = mongoose.Schema;
+var schema = mongoose.Schema
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
 
+//create schema model
 var userSchema = new schema( {
 	username: { type: String, required: true, index: { unique: true } },
 	password: { type: String, required: true }
 });
 
+//mongoose middleware automatically hashes passsword before saving to mongodb
+userSchema.pre( 'save', function( next ) {
+var user = this;
+
+// only hash the password if it has been modified (or is new)
+if ( !user.isModified( 'password' )) return next();
+
+	// generate a salt (used to hash prevents brute force attacks)
+	bcrypt.genSalt( SALT_WORK_FACTOR, function( err, salt ) {
+	if ( err ) return next( err );
+
+        	// hash the password along with our new salt
+		bcrypt.hash( user.password, salt, function( err, hash ) {
+		if ( err ) return next( err );
+
+            	// override the cleartext password with the hashed one
+		user.password = hash;
+		next();
+        	});
+	});
+});
+
+//password verification
+userSchema.methods.comparePassword = function( candidatePassword, cb ) {
+	bcrypt.compare( candidatePassword, this.password, function( err, isMatch ) {
+		if ( err ) return cb( err );
+		cb( null, isMatch );
+    	});
+};
+
+//export this model
 module.exports = mongoose.model( 'user', userSchema );
+
+
+
+
