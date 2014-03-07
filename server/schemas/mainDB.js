32d9ffb85625a07eccf620078d826c2310af10e8
Peter Sussman <the8thbit@gmail.com>
2014-03-07 04:34:36 -0600

    switched to deploy mode

--- server/schemas/mainDB.js
+++ server/schemas/mainDB.js
@@ -8,11 +8,11 @@ if( config.MONGO_USER && config.MONGO_PASS ) {
 	console.log( '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' );
 	console.log( 'mongodb://' + config.MONGO_USER + ':' + config.MONGO_PASS + '@' + config.MONGO_IP + ':' + config.MONGO_PORT );
 	console.log( '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>' );
-	mongoose.connect( 'mongodb://' + config.MONGO_USER + ':' + config.MONGO_PASS + '@' + config.MONGO_IP + ':' + config.MONGO_PORT );
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + ':' + 'p7Ng_cnyHxAw' + '@' + config.MONGO_IP + ':' + config.MONGO_PORT + '/435db' );
 } else if( config.MONGO_USER ) {
-	mongoose.connect( 'mongodb://' + config.MONGO_USER + '@' + config.MONGO_IP + ':' + config.MONGO_PORT + '/chatappdb' );
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + '@' + config.MONGO_IP + ':' + config.MONGO_PORT + '/435db' );
 } else {
-	mongoose.connect( 'mongodb://' + config.MONGO_IP + ':' + config.MONGO_PORT + '/chatappdb' );
+	mongoose.connect( 'mongodb://' + config.MONGO_IP + ':' + config.MONGO_PORT + '/435db' );
 }
 
 var mongo = mongoose.connection;
