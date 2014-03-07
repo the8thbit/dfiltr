7cb1a8f3eec231ea149ae8343e571972930aef50
Peter Sussman <the8thbit@gmail.com>
2014-03-06 21:38:09 -0600

    switched to deploy mode

--- server/schemas/mainDB.js
+++ server/schemas/mainDB.js
@@ -4,7 +4,13 @@ var config  = require( './../../config.js' );
 //ESTABLISH CONNECTION
 //=============================================================================
 var mongoose = require( 'mongoose' );
-mongoose.connect( config.MONGO_IP + ':' + config.MONGO_PORT + '/chatappdb' );
+if( config.MONGO_USER && config.MONGO_PASS ) {
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + ':' + config.MONGO_PASS + '@' + config.MONGO_IP + ':' + config.MONGO_PORT );
+} else if( config.MONGO_USER ) {
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + '@' + config.MONGO_IP + ':' + config.MONGO_PORT + '/chatappdb' );
+} else {
+	mongoose.connect( 'mongodb://' + config.MONGO_IP + ':' + config.MONGO_PORT + '/chatappdb' );
+}
 
 var mongo = mongoose.connection;
 mongo.on( 'error', console.error.bind( console, 'mongo error:' ) );
