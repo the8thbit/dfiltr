9a79e48662277292cecde8dc1028493a629047b5
Peter Sussman <the8thbit@gmail.com>
2014-03-07 21:26:57 -0600

    made registration/login work on openshift

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 //============================================//
 //            SERVER CONFIGURATION            //
 //============================================//
-  if( process.env ) {                         // TOGGLE ON FOR OPENSHIFT DEPLOY
+//if( process.env ) {                         // TOGGLE ON FOR OPENSHIFT DEPLOY
 //============================================//
 //IP ADDRESS                                  //
   var SERVER_IP =                             //
-//'localhost';                                // toggle on for local testing
-  process.env.OPENSHIFT_NODEJS_IP;            // toggle on for openshift deploy
+  'localhost';                                // toggle on for local testing
+//process.env.OPENSHIFT_NODEJS_IP;            // toggle on for openshift deploy
 //============================================//
 //PORT NUMBER                                 //
   var SERVER_PORT =                           //
-//8080;                                       // toggle on for local testing
-  process.env.OPENSHIFT_NODEJS_PORT;          // toggle on for openshift deploy
+  8080;                                       // toggle on for local testing
+//process.env.OPENSHIFT_NODEJS_PORT;          // toggle on for openshift deploy
 //============================================//
 //MONGODB CONNECTION INFO                     //
   var MONGO_USER =                            //
-//''                                          // toggle on for local testing
-  process.env.OPENSHIFT_MONGODB_DB_USERNAME;  // toggle on for openshift deploy
+  null                                        // toggle on for local testing
+//process.env.OPENSHIFT_MONGODB_DB_USERNAME;  // toggle on for openshift deploy
 //============================================//
   var MONGO_PASS =                            //
-//''                                          // toggle on for local testing
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD;  // toggle on for openshift deploy
+  null                                        // toggle on for local testing
+//process.env.OPENSHIFT_MONGODB_DB_PASSWORD;  // toggle on for openshift deploy
 //============================================//
   var MONGO_IP =                              //
-//'localhost'                                 // toggle on for local testing
-  process.env.OPENSHIFT_MONGODB_DB_HOST;      // toggle on for openshift deploy
+  'localhost'                                 // toggle on for local testing
+//process.env.OPENSHIFT_MONGODB_DB_HOST;      // toggle on for openshift deploy
 //============================================//
   var MONGO_PORT =                            //
-//27017                                       // toggle on for local testing
-  process.env.OPENSHIFT_MONGODB_DB_PORT;      //
+  27017                                       // toggle on for local testing
+//process.env.OPENSHIFT_MONGODB_DB_PORT;      // toggle on for openshift deploy
 //============================================//
-  }                                           // TOGGLE ON FOR OPENSHIFT DEPLOY
+//}                                           // TOGGLE ON FOR OPENSHIFT DEPLOY
 //============================================//
 
 //============================================//
@@ -38,13 +38,13 @@
 //============================================//
 //IP ADDRESS                                  //
   var CLIENT_IP =                             //
-//'localhost';                                // toggle on for local testing
-  '435-teamnoname.rhcloud.com';               // toggle on for openshift deploy
+  'localhost';                                // toggle on for local testing
+//'435-teamnoname.rhcloud.com';               // toggle on for openshift deploy
 //============================================//
 //PORT NUMBER                                 //
   var CLIENT_PORT =                           //
-//8080;                                       // toggle on for local testing
-  8000;                                       // neccessary for openshift, as websockets is restricted to this port
+  8080;                                       // toggle on for local testing
+//8000;                                       // neccessary for openshift, as websockets is restricted to this port
 //============================================//
 
 
