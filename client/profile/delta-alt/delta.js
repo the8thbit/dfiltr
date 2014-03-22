b1041e8c267e9b7799006a82fa4555435f7057e3
Peter Sussman <the8thbit@gmail.com>
2014-03-22 00:21:46 -0500

    switched to deploy mode

---
+++ client/profile/delta-alt/delta.js
@@ -0,0 +1,7 @@
+var delta = $( '#delta' );
+$( '#delta-convo-1' ).load( '/modules/convo/' );
+$( '#delta-convo-2' ).load( '/modules/convo/' );
+$( '#delta-convo-3' ).load( '/modules/convo/' );
+$( '#delta-convo-4' ).load( '/modules/convo/' );
+$( '#delta-convo-5' ).load( '/modules/convo/' );
+$( '#delta-convo-6' ).load( '/modules/convo/' );
--- config.js
+++ config.js
@@ -5,48 +5,48 @@ if( process.env ) {                           //
 //============================================//
 //IP ADDRESS                                  //
   var SERVER_IP =                             //
-  'localhost'                                 // toggle on for local testing
-//process.env.OPENSHIFT_NODEJS_IP             // toggle on for openshift deploy
+//'localhost'                                 // toggle on for local testing
+  process.env.OPENSHIFT_NODEJS_IP             // toggle on for openshift deploy
 //============================================//
 //PORT NUMBER                                 //
   var SERVER_PORT =                           //
-  8080                                        // toggle on for local testing
-//process.env.OPENSHIFT_NODEJS_PORT           // toggle on for openshift deploy
+//8080                                        // toggle on for local testing
+  process.env.OPENSHIFT_NODEJS_PORT           // toggle on for openshift deploy
 //============================================//
 //SECRET FOR SESSION DATA                     //
   var COOKIE_SECRET =                         //
-  process.env.DFILT_COOKIE_SECRET             // toggle on for local testing
-//process.env.OPENSHIFT_COOKIE_SECRET         // toggle on for openshift deploy
+//process.env.DFILT_COOKIE_SECRET             // toggle on for local testing
+  process.env.OPENSHIFT_COOKIE_SECRET         // toggle on for openshift deploy
 //============================================//
 //MONGODB CONNECTION INFO                     //
   var MONGO_USER =                            //
-  null                                        // toggle on for local testing
-//process.env.OPENSHIFT_MONGODB_DB_USERNAME   // toggle on for openshift deploy
+//null                                        // toggle on for local testing
+  process.env.OPENSHIFT_MONGODB_DB_USERNAME   // toggle on for openshift deploy
 //============================================//
   var MONGO_PASS =                            //
-  null                                        // toggle on for local testing
-//process.env.OPENSHIFT_MONGODB_DB_PASSWORD   // toggle on for openshift deploy
+//null                                        // toggle on for local testing
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD   // toggle on for openshift deploy
 //============================================//
   var MONGO_IP =                              //
-  'localhost'                                 // toggle on for local testing
-//process.env.OPENSHIFT_MONGODB_DB_HOST       // toggle on for openshift deploy
+//'localhost'                                 // toggle on for local testing
+  process.env.OPENSHIFT_MONGODB_DB_HOST       // toggle on for openshift deploy
 //============================================//
   var MONGO_PORT =                            //
-  27017                                       // toggle on for local testing
-//process.env.OPENSHIFT_MONGODB_DB_PORT       // toggle on for openshift deploy
+//27017                                       // toggle on for local testing
+  process.env.OPENSHIFT_MONGODB_DB_PORT       // toggle on for openshift deploy
 //============================================//
 //PREDICTIONIO CONNECTION INFO                //
   var PIO_API_KEY =                           //
-  process.env.DFILT_PIO_API_KEY               // toggle on for local testing
-//process.env.OPENSHIFT_PIO_API_KEY           // toggle on for openshift deploy
+//process.env.DFILT_PIO_API_KEY               // toggle on for local testing
+  process.env.OPENSHIFT_PIO_API_KEY           // toggle on for openshift deploy
 //============================================//
   var PIO_API_IP =                            //
-  process.env.DFILT_PIO_API_IP                // toggle on for local testing
-//process.env.OPENSHIFT_PIO_API_IP            // toggle on for openshift deploy
+//process.env.DFILT_PIO_API_IP                // toggle on for local testing
+  process.env.OPENSHIFT_PIO_API_IP            // toggle on for openshift deploy
 //============================================//
   var PIO_API_PORT =                          //
-  process.env.DFILT_PIO_API_PORT              // toggle on for local testing
-//process.env.OPENSHIFT_PIO_API_PORT          // toggle on for openshift deploy
+//process.env.DFILT_PIO_API_PORT              // toggle on for local testing
+  process.env.OPENSHIFT_PIO_API_PORT          // toggle on for openshift deploy
 //============================================//
 }                                             //
 //============================================//
@@ -56,13 +56,13 @@ if( process.env ) {                           //
 //============================================//
 //IP ADDRESS                                  //
   var CLIENT_IP =                             //
-  'localhost'                                 // toggle on for local testing
-//'435-teamnoname.rhcloud.com'                // toggle on for openshift deploy
+//'localhost'                                 // toggle on for local testing
+  '435-teamnoname.rhcloud.com'                // toggle on for openshift deploy
 //============================================//
 //PORT NUMBER                                 //
   var CLIENT_PORT =                           //
-  8080                                        // toggle on for local testing
-//8000                                        // neccessary for openshift, as websockets is restricted to this port
+//8080                                        // toggle on for local testing
+  8000                                        // neccessary for openshift, as websockets is restricted to this port
 //============================================//
 
 
