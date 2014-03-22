cc722aabc797840bdaf9d1d53590f86100465a37
Peter Sussman <the8thbit@gmail.com>
2014-03-22 00:48:18 -0500

    switched to deploy mode

--- server/schemas/mainDB.js
+++ server/schemas/mainDB.js
@@ -5,11 +5,11 @@ var config  = require( './../../config.js' );
 //=============================================================================
 var mongoose = require( 'mongoose' );
 if( config.MONGO_USER && config.MONGO_PASS ) {
-	mongoose.connect( 'mongodb://' + config.MONGO_USER + ':' + config.MONGO_PASS + '@' + config.MONGO_IP + ':' + config.MONGO_PORT  + '/435db' );
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + ':' + config.MONGO_PASS + '@' + config.MONGO_IP + ':' + config.MONGO_PORT );
 } else if( config.MONGO_USER ) {
-	mongoose.connect( 'mongodb://' + config.MONGO_USER + '@' + config.MONGO_IP + ':' + config.MONGO_PORT + '/435db' );
+	mongoose.connect( 'mongodb://' + config.MONGO_USER + '@' + config.MONGO_IP + ':' + config.MONGO_PORT );
 } else {
-	mongoose.connect( 'mongodb://' + config.MONGO_IP + ':' + config.MONGO_PORT + '/435db' );
+	mongoose.connect( 'mongodb://' + config.MONGO_IP + ':' + config.MONGO_PORT );
 }
 
 var mongo = mongoose.connection;
