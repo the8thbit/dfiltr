e5c132d3e3618671f452532015af708e0fc7fc0c
Ariel Godinez <ariel@ariel-Aspire-7741.(none)>
2014-04-15 19:53:34 -0500

    added badges schema

---
+++ server/schemas/messages-schema.js
@@ -0,0 +1,13 @@
+var mongoose = require( 'mongoose' );
+var schema = mongoose.Schema
+
+// Create a schema for our data
+var MessageSchema = new Schema ( {
+sessionId { type: String, required true},
+message:  { type: String, required: true },
+date:     { type: Date}
+});
+
+
+// Use the schema to register a model with MongoDb
+module.exports = mongoose.model( 'Message', MessageSchema )
